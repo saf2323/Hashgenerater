360551d50bdd5b621e5c7c36045500bbab632754
Saf <122736181+saf2323@users.noreply.github.com>
2023-06-02 12:40:05 +0530

    Add files via upload

---
+++ Hashgenerated.py
@@ -0,0 +1,33 @@
+import hashlib
+
+input1 = input('ENTER ANY PASSWORD:')
+
+#md5
+h = hashlib.md5()
+h.update(input1.encode())
+print('md5:',h.hexdigest())
+
+#sha256
+h = hashlib.sha256()
+h.update(input1.encode())
+print('sha256:',h.hexdigest())
+
+#sha224
+h = hashlib.sha224()
+h.update(input1.encode())
+print('sha224:',h.hexdigest())
+
+#sha384
+h = hashlib.sha384()
+h.update(input1.encode())
+print('sha384:',h.hexdigest())
+
+#sha512
+h = hashlib.sha512()
+h.update(input1.encode())
+print('sha512:',h.hexdigest())
+
+#sha1
+h = hashlib.sha1()
+h.update(input1.encode())
+print('sha1:',h.hexdigest())
